b70dbafae12d8c8b79d4cb0fad122d79cd5f316e
jdragon <jdraong9527@gmail.com>
2024-06-26 21:06:29 +0800

    feat: 引入ktorm

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,8 @@ dependencies {
     implementation("io.ktor:ktor-serialization-gson:2.0.3")
     implementation("com.google.code.gson:gson:2.8.9")
     implementation("com.jayway.jsonpath:json-path:2.6.0")
+    implementation("com.jayway.jsonpath:json-path:2.6.0")
+    implementation("org.ktorm:ktorm-core:4.0.0")
 }
 
 tasks.test {
