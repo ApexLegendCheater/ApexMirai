70f6a124bfb1e0675f6e946cdf892d1aa0ba8137
jdragon <jdraong9527@gmail.com>
2024-06-28 14:59:42 +0800

    feat: 引入ktor server

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
+val ktorVersion = "2.3.11"
+val logbackVersion = "1.4.12"
 plugins {
     kotlin("jvm") version "1.9.23"
 }
+
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
@@ -14,14 +17,20 @@ dependencies {
     api(platform("net.mamoe:mirai-bom:2.16.0"))
     api("net.mamoe:mirai-core-api")     // 编译代码使用
     runtimeOnly("net.mamoe:mirai-core") // 运行时使用
-    implementation("io.ktor:ktor-client-core:2.0.3")
-    implementation("io.ktor:ktor-client-cio:2.0.3")
-    implementation("io.ktor:ktor-client-content-negotiation:2.0.3")
-    implementation("io.ktor:ktor-serialization-gson:2.0.3")
     implementation("com.google.code.gson:gson:2.8.9")
     implementation("com.jayway.jsonpath:json-path:2.6.0")
     implementation("org.ktorm:ktorm-core:4.0.0")
     runtimeOnly("mysql:mysql-connector-java:8.0.28")
+
+    implementation("io.ktor:ktor-client-core:$ktorVersion")
+    implementation("io.ktor:ktor-client-cio:$ktorVersion")
+    implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-gson:$ktorVersion")
+    implementation("io.ktor:ktor-server-status-pages:$ktorVersion")
+    implementation("io.ktor:ktor-server-core:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    implementation("io.ktor:ktor-server-config-yaml:$ktorVersion")
 }
 
 tasks.test {
---
+++ src/main/kotlin/TestServer.kt
@@ -0,0 +1,21 @@
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+
+fun main() {
+    val server = embeddedServer(Netty, port = 8080) {
+        routing {
+            get("/") {
+                call.respondText("Hello World!", ContentType.Text.Plain)
+            }
+            get("/demo") {
+                call.respondText("HELLO WORLD!")
+            }
+        }
+    }
+    server.start(wait = true)
+}
